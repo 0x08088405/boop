d530bcb20edb7d65f9009832880c54483fc2e152
Adam <classygopher@gmail.com>
2020-11-17 23:12:24 +0000

    lib: add basic player struct

--- src/lib.rs
+++ src/lib.rs
@@ -8,56 +8,24 @@ pub use resampler::Resampler;
 pub use source::Source;
 pub use stream::OutputStream;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// A basic sound-playing object. When fed to an output stream, will play the samples it contains until it has no more.
+/// If the samples have a different sample rate than the output stream, the output will sound sped up or slowed down.
+/// Use a resampler (such as boop::resampler::Polyphase, or implement your own) to resample it at the correct rate.
+pub struct Player {
+    pub samples: Box<[f32]>,
+    pub channels: usize,
+}
 
-    #[test]
-    fn sinewaves() {
-        // Sinewave generators
-        struct Sinewave440();
-        impl Source for Sinewave440 {
-            fn get_sample(&self, index: usize) -> Option<f32> {
-                if index < 66150 {
-                    Some(((index as f32) * 440.0 * 2.0 * std::f32::consts::PI / 44100.0).sin())
-                } else {
-                    None
-                }
-            }
+impl Source for Player {
+    fn get_sample(&self, index: usize) -> Option<f32> {
+        self.samples.get(index).copied()
+    }
 
-            fn channel_count(&self) -> usize {
-                1
-            }
+    fn channel_count(&self) -> usize {
+        self.channels
+    }
 
-            fn sample_rate(&self) -> usize {
-                44100
-            }
-        }
-
-        struct Sinewave800();
-        impl Source for Sinewave800 {
-            fn get_sample(&self, index: usize) -> Option<f32> {
-                Some((((index / 2) as f32) * 800.0 * 2.0 * std::f32::consts::PI / 22000.0).sin())
-            }
-
-            fn channel_count(&self) -> usize {
-                2
-            }
-
-            fn sample_rate(&self) -> usize {
-                22000
-            }
-        }
-
-        // Set up stream
-        let stream = OutputStream::new().unwrap();
-
-        // Play a 440 Hz 1-channel beep which will stop after 1.5 seconds, then wait 1 second
-        stream.add_source(Sinewave440());
-        std::thread::sleep(std::time::Duration::from_millis(1000));
-
-        // Play an 800 Hz 2-channel beep, then wait 1 second
-        stream.add_source(Sinewave800());
-        std::thread::sleep(std::time::Duration::from_millis(1000));
+    fn sample_rate(&self) -> usize {
+        48000 // This function will probably be removed soon and isn't currently used
     }
 }

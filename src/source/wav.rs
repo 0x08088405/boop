2fff2edb6b2eb2fdb27023d250f7cf873a3bfd4e
Adam <classygopher@gmail.com>
2020-11-17 23:11:13 +0000

    wav: fix get_sample bug (+ fmt)

--- src/source/wav.rs
+++ src/source/wav.rs
@@ -4,7 +4,7 @@ pub struct WavPlayer {
     file: Vec<u8>,
     channels: usize,
     sample_rate: usize,
-    block_align: usize,
+    sample_bytes: usize,
     data_start: usize,
     data_end: usize,
     sample_getter: fn(&[u8], usize) -> Option<f32>,
@@ -25,23 +25,19 @@ pub enum Error {
 impl WavPlayer {
     pub fn new(file: impl Into<Vec<u8>>) -> Result<Self, Error> {
         let mut file = file.into();
-        if file.len() < 36
-            || file[0..4] != [b'R', b'I', b'F', b'F']
-            || file[8..12] != [b'W', b'A', b'V', b'E']
-        {
-            return Err(Error::InvalidFile);
+        if file.len() < 36 || file[0..4] != [b'R', b'I', b'F', b'F'] || file[8..12] != [b'W', b'A', b'V', b'E'] {
+            return Err(Error::InvalidFile)
         }
 
         let audio_format = i16::from_le_bytes([file[20], file[21]]);
         let channels = u16::from_le_bytes([file[22], file[23]]);
         let sample_rate = u32::from_le_bytes([file[24], file[25], file[26], file[27]]);
-        let block_align = u16::from_le_bytes([file[32], file[33]]);
         let sample_bits = u16::from_le_bytes([file[34], file[35]]);
 
         let mut data_start: usize = 36;
         let data_len = loop {
             if file.len() < data_start + 8 {
-                return Err(Error::InvalidFile);
+                return Err(Error::InvalidFile)
             }
             let is_data_chunk = file[data_start..(data_start + 4)] == [b'd', b'a', b't', b'a'];
             let data_len = u32::from_le_bytes([
@@ -52,7 +48,7 @@ impl WavPlayer {
             ]) as usize;
             data_start += 8;
             if is_data_chunk {
-                break data_len;
+                break data_len
             } else {
                 data_start += data_len;
             }
@@ -60,7 +56,7 @@ impl WavPlayer {
 
         let expected_file_length = data_len + data_start;
         if expected_file_length > file.len() {
-            return Err(Error::MalformedData);
+            return Err(Error::MalformedData)
         } else {
             file.truncate(expected_file_length);
         }
@@ -78,7 +74,7 @@ impl WavPlayer {
             file,
             channels: channels.into(),
             sample_rate: sample_rate as usize,
-            block_align: block_align.into(),
+            sample_bytes: usize::from(sample_bits / 8),
             data_start,
             data_end: data_start + data_len,
             sample_getter,
@@ -88,7 +84,7 @@ impl WavPlayer {
 
 impl Source for WavPlayer {
     fn get_sample(&self, index: usize) -> Option<f32> {
-        let offset = index * self.block_align;
+        let offset = index * self.sample_bytes;
         (self.sample_getter)(&self.file[self.data_start..self.data_end], offset)
     }
 

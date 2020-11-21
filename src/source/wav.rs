833d98e06f69a6b046d625fed0e9e7f5c8d1edac
Adam <classygopher@gmail.com>
2020-11-21 21:02:01 +0000

    wav: optimize wav parsing

--- src/source/wav.rs
+++ src/source/wav.rs
@@ -1,13 +1,14 @@
 use super::Source;
 
+#[derive(Clone, Debug)]
 pub struct WavPlayer {
     file: Vec<u8>,
     channels: usize,
     sample_rate: usize,
     sample_bytes: usize,
     next_sample_offset: usize,
-    data_end: usize,
-    sample_getter: fn(&[u8], usize) -> Option<f32>,
+    format: Format,
+    length: usize,
 }
 
 #[derive(Clone, Copy, Debug)]
@@ -20,6 +21,15 @@ pub enum Error {
 
     /// The audio data in this file is encoded in a way we don't support
     UnknownFormat,
+}
+
+#[derive(Clone, Copy, Debug)]
+pub enum Format {
+    U8,
+    I16,
+    I24,
+    I32,
+    F32,
 }
 
 impl WavPlayer {
@@ -61,29 +71,31 @@ impl WavPlayer {
             file.truncate(expected_file_length);
         }
 
-        let sample_getter = match (audio_format, sample_bits) {
-            (1, 8) => get_sample_u8,
-            (1, 16) => get_sample_i16,
-            (1, 24) => get_sample_i24,
-            (1, 32) => get_sample_i32,
-            (3, 32) => get_sample_f32,
+        let format = match (audio_format, sample_bits) {
+            (1, 8) => Format::U8,
+            (1, 16) => Format::I16,
+            (1, 24) => Format::I24,
+            (1, 32) => Format::I32,
+            (3, 32) => Format::F32,
             _ => return Err(Error::UnknownFormat),
         };
+
+        let sample_bytes = usize::from(sample_bits / 8);
 
         Ok(Self {
             file,
             channels: channels.into(),
             sample_rate: sample_rate as usize,
-            sample_bytes: usize::from(sample_bits / 8),
+            sample_bytes,
             next_sample_offset: data_start,
-            data_end: data_start + data_len,
-            sample_getter,
+            format,
+            length: data_len / sample_bytes,
         })
     }
 
-    /// Returns the total number of samples remaining in this wav file
+    /// Returns the total number of samples in this wav file
     pub fn length(&self) -> usize {
-        (self.data_end - self.next_sample_offset) / self.sample_bytes
+        self.length
     }
 
     /// Returns the sample rate of this wav file (eg. 44100)
@@ -94,15 +106,49 @@ impl WavPlayer {
 
 impl Source for WavPlayer {
     fn write_samples(&mut self, buffer: &mut [f32]) -> usize {
-        for (i, out) in buffer.iter_mut().enumerate() {
-            if let Some(s) = (self.sample_getter)(&self.file[self.next_sample_offset..self.data_end], 0) {
-                *out = s;
-                self.next_sample_offset += self.sample_bytes;
-            } else {
-                return i
+        use std::convert::TryInto;
+
+        if let Some(i) = self.file.get(self.next_sample_offset..) {
+            let output_iter = buffer.iter_mut();
+
+            let samples_written;
+            match self.format {
+                Format::U8 => {
+                    let iter = output_iter.zip(i.iter().copied());
+                    samples_written = iter.len();
+                    iter.for_each(|(out, b)| *out = get_sample_u8(b));
+                },
+                Format::I16 => {
+                    let iter =
+                        output_iter.zip(i.chunks_exact(2).map(|x| <&[u8] as TryInto<&[u8; 2]>>::try_into(x).unwrap()));
+                    samples_written = iter.len();
+                    iter.for_each(|(out, b)| *out = get_sample_i16(b));
+                },
+                Format::I24 => {
+                    let iter =
+                        output_iter.zip(i.chunks_exact(3).map(|x| <&[u8] as TryInto<&[u8; 3]>>::try_into(x).unwrap()));
+                    samples_written = iter.len();
+                    iter.for_each(|(out, b)| *out = get_sample_i24(b));
+                },
+                Format::I32 => {
+                    let iter =
+                        output_iter.zip(i.chunks_exact(4).map(|x| <&[u8] as TryInto<&[u8; 4]>>::try_into(x).unwrap()));
+                    samples_written = iter.len();
+                    iter.for_each(|(out, b)| *out = get_sample_i32(b));
+                },
+                Format::F32 => {
+                    let iter =
+                        output_iter.zip(i.chunks_exact(4).map(|x| <&[u8] as TryInto<&[u8; 4]>>::try_into(x).unwrap()));
+                    samples_written = iter.len();
+                    iter.for_each(|(out, b)| *out = get_sample_f32(b));
+                },
             }
+
+            self.next_sample_offset += samples_written * self.sample_bytes;
+            samples_written
+        } else {
+            0
         }
-        buffer.len()
     }
 
     fn channel_count(&self) -> usize {
@@ -110,42 +156,31 @@ impl Source for WavPlayer {
     }
 }
 
-fn get_sample_u8(data: &[u8], offset: usize) -> Option<f32> {
-    let sample = data.get(offset).copied().map(i16::from)? - 0x80;
-    Some(f32::from(sample) / f32::from(i8::MAX))
+#[inline(always)]
+fn get_sample_u8(data: u8) -> f32 {
+    let sample = i16::from(data) - 0x80;
+    f32::from(sample) / f32::from(i8::MAX)
 }
 
-fn get_sample_i16(data: &[u8], offset: usize) -> Option<f32> {
-    let sample = i16::from_le_bytes([data.get(offset).copied()?, data.get(offset + 1).copied()?]);
-    Some(f32::from(sample) / f32::from(i16::MAX))
+#[inline(always)]
+fn get_sample_i16(data: &[u8; 2]) -> f32 {
+    let sample = i16::from_le_bytes(*data);
+    f32::from(sample) / f32::from(i16::MAX)
 }
 
-fn get_sample_i24(data: &[u8], offset: usize) -> Option<f32> {
-    let sample = i32::from_le_bytes([
-        data.get(offset).copied()?,
-        data.get(offset + 1).copied()?,
-        data.get(offset + 2).copied()?,
-        0,
-    ]);
-    Some((sample as f32) / 8388608.0)
+#[inline(always)]
+fn get_sample_i24(data: &[u8; 3]) -> f32 {
+    let sample = i32::from_le_bytes([data[0], data[1], data[2], 0]);
+    (sample as f32) / 8388608.0 // 2^23, or the imaginary i24::MAX
 }
 
-fn get_sample_i32(data: &[u8], offset: usize) -> Option<f32> {
-    let sample = i32::from_le_bytes([
-        data.get(offset).copied()?,
-        data.get(offset + 1).copied()?,
-        data.get(offset + 2).copied()?,
-        data.get(offset + 3).copied()?,
-    ]);
-    Some((f64::from(sample) / f64::from(i32::MAX)) as f32)
+#[inline(always)]
+fn get_sample_i32(data: &[u8; 4]) -> f32 {
+    let sample = i32::from_le_bytes(*data);
+    (f64::from(sample) / f64::from(i32::MAX)) as f32
 }
 
-fn get_sample_f32(data: &[u8], offset: usize) -> Option<f32> {
-    let sample = f32::from_le_bytes([
-        data.get(offset).copied()?,
-        data.get(offset + 1).copied()?,
-        data.get(offset + 2).copied()?,
-        data.get(offset + 3).copied()?,
-    ]);
-    Some(sample)
+#[inline(always)]
+fn get_sample_f32(data: &[u8; 4]) -> f32 {
+    f32::from_le_bytes(*data)
 }
